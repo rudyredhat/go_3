12b221eeeb483446fab266973b8366b0170174d2
rudyredhat <rs9046496998@gmail.com>
2021-01-07 23:12:54 +0530

    l6-13.1 updates

--- L6/13/fibonacci.go
+++ L6/13/fibonacci.go
@@ -24,3 +24,15 @@ func fibonacci(n int) (res int) {
 	}
 	return
 }
+
+// fibonacci(0) is: 1
+// fibonacci(1) is: 1
+// fibonacci(2) is: 2
+// fibonacci(3) is: 3
+// fibonacci(4) is: 5
+// fibonacci(5) is: 8
+// fibonacci(6) is: 13
+// fibonacci(7) is: 21
+// fibonacci(8) is: 34
+// fibonacci(9) is: 55
+// fibonacci(10) is: 89
